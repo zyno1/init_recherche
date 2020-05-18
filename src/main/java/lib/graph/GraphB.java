4562b36ef2094913df771419d92b2c2469475319
zyno1 <olivierzeyen@gmail.com>
2020-05-18 21:36:28 +0200

    merge

--- src/main/java/lib/graph/GraphB.java
+++ src/main/java/lib/graph/GraphB.java
@@ -1,6 +1,7 @@
 package lib.graph;
 
 import lib.exceptions.InvalidOperationException;
+import lib.math.Calcul;
 
 import java.util.ArrayList;
 
@@ -29,6 +30,26 @@ public class GraphB implements IGraph {
 
     public void setEdgeCount(int i1, int i2, int n) {
         data.set(i1 * nb + i2, n);
+    }
+
+    public int[] getExits(int i) {
+        int[] res = new int[nb];
+
+        for(int j = 0; j < nb; j++) {
+            res[j] = getEdgeCount(i, j);
+        }
+
+        return res;
+    }
+
+    public int[] getEntries(int i) {
+        int [] res = new int[nb];
+
+        for(int j = 0; j < nb; j++) {
+            res[j] = getEdgeCount(j, i);
+        }
+
+        return res;
     }
 
     public void addEdges(int i1, int i2, int nb) {
@@ -63,5 +84,126 @@ public class GraphB implements IGraph {
             data.remove(j);
         }
         nb--;
+    }
+
+    public int splitExits(int i1, int... split) throws InvalidOperationException {
+        for (int i = 0; i < nbVertices() && i < split.length; i++) {
+            if(getEdgeCount(i1, i) < split[i]) {
+                throw new InvalidOperationException();
+            }
+        }
+
+        int i2 = addNode();
+        addEdges(i1, i2, 1);
+
+        for(int j = 0; j < nbVertices() && j < split.length; j++) {
+            removeEdges(i1, j, split[j]);
+            addEdges(i2, j, split[j]);
+        }
+
+        return i2;
+    }
+
+    public int splitEntries(int i1, int... split) throws InvalidOperationException {
+        for (int i = 0; i < nbVertices() && i < split.length; i++) {
+            if(getEdgeCount(i, i1) < split[i]) {
+                throw new InvalidOperationException();
+            }
+        }
+
+        int i2 = addNode();
+        addEdges(i2, i1, 1);
+
+        for(int j = 0; j < nbVertices() && j < split.length; j++) {
+            removeEdges(j, i1, split[j]);
+            addEdges(j, i2, split[j]);
+        }
+
+        return i2;
+    }
+
+    private void merge_r2(int i1, int i2) {
+        int node = Math.min(i1, i2);
+
+        for(int j = 0; j < nbVertices(); j++) {
+            int in = getEdgeCount(j, i1) + getEdgeCount(j, i2);
+            int out = getEdgeCount(i1, j) + getEdgeCount(i2, j);
+
+            setEdgeCount(j, node, in);
+            setEdgeCount(node, j, out);
+        }
+
+        removeNode(Math.max(i1, i2));
+    }
+
+    private void merge_on_entry(int i1, int i2) {
+        int node = Math.min(i1, i2);
+
+        for(int j = 0; j < nbVertices(); j++) {
+            int in = getEdgeCount(j, i1);
+            int out = getEdgeCount(i1, j) + getEdgeCount(i2, j);
+
+            setEdgeCount(j, node, in);
+            setEdgeCount(node, j, out);
+        }
+
+        removeNode(Math.max(i1, i2));
+    }
+
+    private void merge_on_exit(int i1, int i2) {
+        int node = Math.min(i1, i2);
+
+        for(int j = 0; j < nbVertices(); j++) {
+            int in = getEdgeCount(j, i1) + getEdgeCount(j, i2);
+            int out = getEdgeCount(i1, j);
+
+            setEdgeCount(j, node, in);
+            setEdgeCount(node, j, out);
+        }
+
+        removeNode(Math.max(i1, i2));
+    }
+
+    public void merge(int i1, int i2) throws InvalidOperationException {
+        if(getEdgeCount(i1, i2) == 1) {
+            if(Calcul.sum(getExits(i1)) == 1) {
+                merge_r2(i1, i2);
+            }
+            else if(Calcul.sum(getEntries(i2)) == 1) {
+                merge_r2(i1, i2);
+            }
+            else {
+                throw new InvalidOperationException();
+            }
+        }
+        else if(getEdgeCount(i2, i1) == 1) {
+            if(Calcul.sum(getExits(i2)) == 1) {
+                merge_r2(i1, i2);
+            }
+            else if(Calcul.sum(getEntries(i1)) == 1) {
+                merge_r2(i1, i2);
+            }
+            else {
+                throw new InvalidOperationException();
+            }
+        }
+        else {
+            boolean enEq = true;
+            boolean exEq = true;
+            for(int i = 0; i < nbVertices() && enEq && exEq; i++) {
+                enEq = enEq && getEdgeCount(i, i1) == getEdgeCount(i, i2);
+                exEq = exEq && getEdgeCount(i1, i) == getEdgeCount(i2, i);
+            }
+
+            if(enEq) {
+                merge_on_entry(i1, i2);
+            }
+            else if(exEq) {
+                merge_on_exit(i1, i2);
+            }
+            else {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }
